93bc89633abe6eb5be62ec325b3fb6299127c45c
FRANKSADWAR <billyfranks98@gmail.com>
2025-03-13 16:55:13 +0300

    ageing report

--- ideal_customs/report_generator.py
+++ ideal_customs/report_generator.py
@@ -21,5 +21,19 @@ def get_report(customer: str):
     return report
 
 @frappe.whitelist(allow_guest= True)
-def get_receivable_summary(allow_guest = True):
-    pass
+def get_receivable_summary(customer: str):
+    company_list = frappe.db.get_list('Company')
+    company = company_list[0].name
+    filters = {
+        "company": company,
+        "party_type": "Customer",
+        "party" : [customer],
+        "range1": 30,
+        "range2": 60,
+        "range3": 90,
+        "range4": 120,
+        "range5": 360,
+        "show_remarks" : False
+    }
+    ageing_report = execute(filters)
+    return ageing_report
